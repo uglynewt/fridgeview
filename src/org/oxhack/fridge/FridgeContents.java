0fa8d342140f899e88961485b8d548ba4d199c61
Glyn Kennington <glyn@potatojunkie.co.uk>
2015-01-07 23:24:34 +0000

    Read list from a file, instead of encoding it in the class file.
    At the moment it's a raw resource file.  This will eventually
    need to be moved to a writeable location that can be updated.

--- src/org/oxhack/fridge/FridgeContents.java
+++ src/org/oxhack/fridge/FridgeContents.java
@@ -7,6 +7,11 @@ import android.widget.TextView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.content.Context;
+import android.content.res.Resources;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 
 public class FridgeContents extends Activity
@@ -44,63 +49,31 @@ public class FridgeContents extends Activity
 	TableLayout pricelist = (TableLayout) findViewById(R.id.pricelist);
 
 	pricelist.setColumnStretchable(0,true);
-	/* TODO: read this list from file */
 
-	addrow(this, pricelist, "Soft Drinks", "");
-	addrow(this, pricelist, "Soft drink cans (not San Pelleg.)", "0.75");
-	addrow(this, pricelist, "San Pellegrino soft drinks", "1.00");
-	addrow(this, pricelist, "San Pellegrino sparkling water", "0.75");
-	addrow(this, pricelist, "Bottled Coca-Cola", "1.00");
-	addrow(this, pricelist, "Mojito soft drinks (750ml)", "2.00");
-	addrow(this, pricelist, "Thai iced tea (canned)", "1.20");
-	addrow(this, pricelist, "Foco Coconut juice (canned)", "1.20");
-	addrow(this, pricelist, "J20 (bottles)", "1.00");
-	addrow(this, pricelist, "Capri-Sun orange juice", "0.50");
-	addrow(this, pricelist, "Fentimans drinks (bottles)", "1.75");
-	addrow(this, pricelist, "Fever-Tree ginger beer (bottles)", "2.20");
-	addrow(this, pricelist, "Fever-Tree tonic water (bottles)", "2.20");
-	addrow(this, pricelist, "Highland Spring Still / Sparkling", "0.75");
-	addrow(this, pricelist, "A&W Root Beer", "0.75");
-	addrow(this, pricelist, "Energy drinks", "");
-	addrow(this, pricelist, "Monster energy drinks (canned)", "1.50");
-	addrow(this, pricelist, "Club Mate", "2.00");
-	addrow(this, pricelist, "Lucozade sports drinks", "1.50");
-	addrow(this, pricelist, "Red Bull (250ml)", "1.25");
-	addrow(this, pricelist, "Alcoholic drinks", "");
-	addrow(this, pricelist, "Real ale (bottled)", "2.50");
-	addrow(this, pricelist, "Cider (bottled)", "2.50");
-	addrow(this, pricelist, "Lagers", "1.50");
-	addrow(this, pricelist, "Lagers (660ml to 1l)", "2.50");
-	addrow(this, pricelist, "Wine", "7.00");
-	addrow(this, pricelist, "Small wine (shiraz, 180ml)", "3.00");
-	addrow(this, pricelist, "Bavaria Shandy", "0.75");
-	addrow(this, pricelist, "Alcohol-free beer", "");
-	addrow(this, pricelist, "Bavaria non-alcoholic beer", "0.75");
-	addrow(this, pricelist, "Becks Blue non-alcoholic beer (275ml)", "0.75");
-	addrow(this, pricelist, "Hopper softbrew (bottles)", "1.50");
-	addrow(this, pricelist, "Food", "");
-	addrow(this, pricelist, "Basic crisps", "0.40");
-	addrow(this, pricelist, "Most chocolate bars", "0.75");
-	addrow(this, pricelist, "Smarties", "0.75");
-	addrow(this, pricelist, "Nestle Milkybar", "0.50");
-	addrow(this, pricelist, "Small Milky Ways", "0.40");
-	addrow(this, pricelist, "Starburst", "0.75");
-	addrow(this, pricelist, "Skittles, 55g", "0.75");
-	addrow(this, pricelist, "Baby Pringles", "0.75");
-	addrow(this, pricelist, "Oreos", "0.75");
-	addrow(this, pricelist, "Turkish Delight", "0.50");
-	addrow(this, pricelist, "Puddings", "0.40");
-	addrow(this, pricelist, "Alpro/Provamel soya puddings", "0.50");
-	addrow(this, pricelist, "Nature Valley bars", "0.40");
-	addrow(this, pricelist, "Nakd fruit bars", "1.00");
-	addrow(this, pricelist, "Small fruit & nut bags", "0.40");
-	addrow(this, pricelist, "Strawberry laces", "0.50");
-	addrow(this, pricelist, "Gummy bears", "0.50");
-	addrow(this, pricelist, "Chocolate orange", "3.50");
-	addrow(this, pricelist, "Kinder Surprise eggs", "1.00");
-	addrow(this, pricelist, "Ice cream on a stick", "0.50");
-	addrow(this, pricelist, "Sainsbury's pizzas", "3.00");
-	addrow(this, pricelist, "Soups (on top of the fridge)", "1.00");
+	/* now read every item from the file */
+	BufferedReader reader = new BufferedReader(new InputStreamReader(
+				getResources().openRawResource(R.raw.contents)
+				));
+	if (reader == null) {
+		addrow(this, pricelist, "Error creating reader", "");
+		return;
+	}
+	String line;
+	try {
+		while ((line = reader.readLine()) != null) {
+			String lr[];
+			lr = line.split(":",2);
+			if (lr.length < 2) {
+				continue;
+			}
+			
+			addrow(this, pricelist, lr[0], lr[1]);
+		}
+		reader.close();
+	}
+	catch (IOException io) {
+		addrow(this, pricelist, "Error reading rest of list", "");
+	}
 
     }
 
